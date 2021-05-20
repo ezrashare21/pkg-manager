ba78328c9d1b6550ea091380885dd5dc1d9e7144
ezrashare21 <79548308+ezrashare21@users.noreply.github.com>
2021-05-19 17:36:50 -0700

    Update pkg_uninstall.py
    copyrights

--- pkg_uninstall.py
+++ pkg_uninstall.py
@@ -2,6 +2,12 @@ from zipfile import ZipFile
 from pathlib import Path as path_lib
 import os
 
+#
+# MIT License
+#
+# Copyright (c) 2021 ezrashare21
+#
+
 def cleanDir(input_):
     print("clearing files....")
     
